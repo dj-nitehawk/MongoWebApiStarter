39075f8b1021e99c76c1b0dccac8d6d80adde194
Damith Gunner <djnitehawk83@gmail.com>
2019-10-09 22:31:01 +0530

    ModelBase.One<T> method added

--- biz/Base/ModelBase.cs
+++ biz/Base/ModelBase.cs
@@ -1,9 +1,18 @@
-﻿namespace App.Biz.Base
+﻿using MongoDB.Entities;
+
+namespace App.Biz.Base
 {
     public abstract class ModelBase<TRepo> where TRepo : new()
     {
         public TRepo Repo { get; set; } = new TRepo();
+
         abstract public void Save();
+
         abstract public void Load();
+
+        public One<TEntity> One<TEntity>(string id) where TEntity : Entity
+        {
+            return new One<TEntity>() { ID = id };
+        }
     }
 }
