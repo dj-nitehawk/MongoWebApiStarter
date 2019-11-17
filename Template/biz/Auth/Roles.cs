57dbcabf026427e8f7b8491d95c4dcbbdc30d2cf
Damith Gunner <djnitehawk83@gmail.com>
2019-11-17 12:39:29 +0530

    refactored roles

--- Template/biz/Auth/Roles.cs
+++ Template/biz/Auth/Roles.cs
@@ -1,9 +1,47 @@
-﻿namespace MongoWebApiStarter.Biz.Auth
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MongoWebApiStarter.Biz.Auth
 {
     public static class Roles
     {
         public const string Admin = "role.admin";
         public const string Owner = "role.owner";
         public const string Employee = "role.employee";
+
+        static Roles()
+        {
+            AllRoles = new Dictionary<string, string>();
+            foreach (var f in typeof(Roles).GetFields())
+            {
+                AllRoles.Add(f.Name, f.GetValue(null) as string);
+            }
+        }
+
+        public static Dictionary<string, string> AllRoles { get; private set; }
+
+        public static string[] GetRoleNames(string[] roleValues)
+        {
+            return AllRoles.Where(r => roleValues.Contains(r.Value))
+                           .Select(r => r.Key)
+                           .ToArray();
+        }
+
+        public static string GetRoleName(string roleValue)
+        {
+            return GetRoleNames(new[] { roleValue }).Single();
+        }
+
+        public static string[] GetRoleValues(string[] roleNames)
+        {
+            return AllRoles.Where(r => roleNames.Contains(r.Key))
+                           .Select(r => r.Value)
+                           .ToArray();
+        }
+
+        public static string GetRoleValue(string roleName)
+        {
+            return GetRoleValues(new[] { roleName }).Single(); ;
+        }
     }
 }
---
+++ Template/test/RolesTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoWebApiStarter.Biz.Auth;
+using FluentAssertions;
+
+namespace MongoWebApiStarter.Test
+{
+    [TestClass]
+    public class RolesTest
+    {
+        [TestMethod]
+        public void role_dictionary_is_not_empty()
+        {
+            Assert.IsTrue(Roles.AllRoles.Count > 0);
+        }
+
+        [TestMethod]
+        public void get_role_names_from_values()
+        {
+            var vals = new[] { Roles.Admin, Roles.Employee };
+            var names = Roles.GetRoleNames(vals);
+
+            names.Length.Should().Be(2);
+            names[0].Should().Be(nameof(Roles.Admin));
+        }
+
+        [TestMethod]
+        public void get_role_values_from_names()
+        {
+            var names = new[] { Roles.GetRoleName(Roles.Admin), Roles.GetRoleName(Roles.Employee) };
+            var vals = Roles.GetRoleValues(names);
+
+            vals.Length.Should().Be(2);
+            vals[0].Should().Be(Roles.Admin);
+        }
+    }
+}
