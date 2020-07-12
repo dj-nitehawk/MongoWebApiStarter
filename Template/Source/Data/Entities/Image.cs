db8c059d4422f9bc7d53266c7e7a9a40ff9d3fa7
Damith Gunner <djnitehawk83@gmail.com>
2020-07-12 12:05:29 +0530

    images to be saved in seperate db

--- Template/Source/Data/Entities/Image.cs
+++ Template/Source/Data/Entities/Image.cs
@@ -3,10 +3,12 @@ using System;
 
 namespace Data
 {
+    [Database(Constants.FileBucketDB)]
     public class Image : FileEntity
     {
         public int Width { get; set; }
         public int Height { get; set; }
         public DateTime AccessedOn { get; set; }
+        public bool IsLinked { get; set; }
     }
 }
--- Template/Source/Data/Repos/Image.cs
+++ Template/Source/Data/Repos/Image.cs
@@ -1,11 +1,15 @@
 ﻿using MongoDB.Entities;
+using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Data
 {
     public class RepoImage : RepoBase<Image>
     {
+        //todo: write tests for uploading and downloading images
+
         public static async Task<string> UploadAsync(Image image, Stream stream)
         {
             image.Save();
@@ -24,8 +28,24 @@ namespace Data
 
             return DB.File<Image>(id)
                      .DownloadWithTimeoutAsync(destination, 30);
+        }
 
-            //todo: write a background service to purge images that haven't been hit in the past 6 months
+        //todo: call this method when account is being saved to link the uploaded image
+        public static void LinkImages(params string[] imageIDs)
+        {
+            if (imageIDs.Any())
+                DB.Update<Image>()
+                  .Match(i => imageIDs.Contains(i.ID))
+                  .Modify(i => i.IsLinked, true)
+                  .Execute();
+
+            //todo: write a background service to purge images that haven't been linked [IsLinked=false] after 24hrs of creation
+        }
+
+        public static void DeleteUnlinkedImages()
+        {
+            var aDayAgo = DateTime.UtcNow.AddDays(-1);
+            _ = DB.DeleteAsync<Image>(i => i.ModifiedOn <= aDayAgo);
         }
     }
 }
